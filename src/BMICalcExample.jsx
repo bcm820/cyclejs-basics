3982b770703ac794700c0c33799fd65b5862c6f0
Brian Mendoza <brian.mendoza@deciphernow.com>
2018-05-09 08:11:14 -0400

    updated notes

--- src/BMICalcExample.jsx
+++ src/BMICalcExample.jsx
@@ -17,46 +17,25 @@ import xs from 'xstream';
 // which flow to the model, transforming state
 // represented in the view, which updates the DOM
 // The cycle is composed as three functions: intent, model, and view
-
 function main(sources) {
-  const actions = intent(sources.DOM);
-  const state$ = model(actions);
-  const vdom$ = view(state$);
+  // const actions = intent(sources.DOM);
+  // const state$ = model(actions);
+  // const vdom$ = view(state$);
   return { DOM: view(model(intent(sources.DOM))) };
 }
 
 function intent(domSource) {
   return {
-    changeWeight$: domSource
-      .select('#Weight')
-      .events('input')
-      .map(event => event.target.value),
-    changeHeight$: domSource
-      .select('#Height')
-      .events('input')
-      .map(event => event.target.value)
+    changeWeight$: readUserAction(domSource, 'Weight'),
+    changeHeight$: readUserAction(domSource, 'Height')
   };
 }
 
-function view(state$) {
-  return state$.map(({ weight, height, bmi }) => (
-    <div>
-      {BMISlider('Weight', 'lbs.', weight, 80, 300)}
-      {BMISlider('Height', 'ft.', height, 150, 250)}
-      <h2>BMI: {bmi}</h2>
-    </div>
-  ));
-}
-
-function BMISlider(title, label, value, min, max) {
-  return (
-    <div>
-      <input type="range" id={title} min={min} max={max} value={value} />
-      {` ${title}: `}
-      {title === 'Height' ? Math.round(value / 30 * 100) / 100 : value}
-      {` ${label} `}
-    </div>
-  );
+function readUserAction(domSource, className) {
+  return domSource
+    .select(`.${className}`)
+    .events('input')
+    .map(event => event.target.value);
 }
 
 function model(actions) {
@@ -72,6 +51,27 @@ function model(actions) {
 function calcBMI(weight, height) {
   const heightIn = height / 2.54;
   return Math.round(weight / heightIn / heightIn * 703 * 100) / 100;
+}
+
+function view(state$) {
+  return state$.map(({ weight, height, bmi }) => (
+    <div>
+      {renderSlider('Weight', 'lbs.', weight, 80, 300)}
+      {renderSlider('Height', 'ft.', height, 150, 250)}
+      <h2>BMI: {bmi}</h2>
+    </div>
+  ));
+}
+
+function renderSlider(label, unit, value, min, max) {
+  return (
+    <div>
+      <input type="range" className={label} min={min} max={max} value={value} />
+      {` ${label}: `}
+      {label === 'Height' ? Math.round(value / 30 * 100) / 100 : value}
+      {` ${unit} `}
+    </div>
+  );
 }
 
 export const drivers = {
